f922bbe2b9832ebee0a89f4e591d2d330dadcc84
David Rosario <darosario@gmail.com>
2024-06-13 19:28:32 -0400

    Next config fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-module.exports = {
-  reactStrictMode: true,
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode:true,
   webpackDevMiddleware: config => {
     config.watchOptions = {
       poll:800,
       aggregateTimeout: 300,
     }
-    return config
-  },
+  }
 }
+ 
+module.exports = nextConfig
+
+
+
